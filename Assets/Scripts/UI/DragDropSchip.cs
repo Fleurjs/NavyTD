52648a34fe40b2cd14698e6aa6d191b54df2e69f
blizzard190 <22256@ma-web.nl>
2021-04-01 10:40:48 +0200

    Feat: Made the prefab chage color
    Made it so that the ship prefab changes color based on if it can be placed or not. This is done with layermask.

--- Assets/Scripts/UI/DragDropSchip.cs
+++ Assets/Scripts/UI/DragDropSchip.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDropSchip : MonoBehaviour, IDragHandler, /*IEndDragHandler,*/ IBeginDragHandler
+public class DragDropSchip : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
 {
     [SerializeField]
     private GameObject _ship;
     private GameObject _target;
+    private Material _targetMat;
+    private int _waterLayer = 1 << 4, _allLayers = 1 << 7;
 
+    private void Awake()
+    {
+        _allLayers = ~_allLayers;
+    }
+
+    //Activates when the player begins the drag. Spawns a preview ship .
     public void OnBeginDrag(PointerEventData eventData)
     {
         RaycastHit hit;
@@ -14,38 +22,42 @@ public class DragDropSchip : MonoBehaviour, IDragHandler, /*IEndDragHandler,*/ I
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             _target = Instantiate(_ship, hit.point, _ship.transform.rotation);
-            Debug.Log("Begin");
+            _targetMat = _target.gameObject.GetComponent<Renderer>().material;
         }
     }
 
+    //Activates every frame of the drag movement updates the color based on layermask check.
     public void OnDrag(PointerEventData eventData)
     {
-        int layermask = 1 << 7;
-        layermask = ~layermask;
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, _allLayers))
         {
             _target.transform.position = hit.point;
+            if(hit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
+                _targetMat.color = Color.blue;
+            else
+                _targetMat.color = Color.red;
         }
     }
 
-    /*public void OnEndDrag(PointerEventData eventData)
+    //Activates when the player ends the drag movement.
+    public void OnEndDrag(PointerEventData eventData)
     {
         Drop();
     }
 
+    //Places or destroys the ship based on layermask.
     private void Drop()
     {
-        int LayerMask = 1 << 4;
         RaycastHit hit;
-        Debug.Log(Input.mousePosition);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, _waterLayer))
         {
             Instantiate(_ship, hit.point, _ship.transform.rotation);
             Destroy(_target);
         }
-        //transform.localPosition = Vector3.zero;
-    }*/
+        else
+            Destroy(_target);
+    }
 }
